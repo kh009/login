bbd4a3427e21ef7cdb878163379c79812cb8979d
kh009 <122242711+kh009@users.noreply.github.com>
2023-12-17 13:26:58 +0800

    12_17

--- loginapp/admin.py
+++ loginapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import *
-from .resources import StudentResource
+from .resources import StudentResource,CourseResource,Course_SelectionResource,TeacherResource,ClassroomResource
 # Register your models here.
 
 admin.site.site_header = '點名系統管理後台'  # 設置標頭
@@ -16,23 +16,25 @@ class StudentAdmin(ImportExportModelAdmin):
 @admin.register(Classroom)
 class ClassroomAdmin(ImportExportModelAdmin):
     list_display = ('classroom_key', 'seat')
-
+    resource_class = ClassroomResource
 
 @admin.register(Teacher)
 class TeacherAdmin(ImportExportModelAdmin):
     list_display = ('teacher_key', 'teacher_name', 'job_title',
                     'teacher_email', 'phone', 'seat')
-
+    resource_class = TeacherResource
 
 @admin.register(Course)
 class CourseAdmin(ImportExportModelAdmin):
     list_display = ('course_key', 'teacher_key', 'course_name',
                     'department', 'course_grade', 'course_className', 'classroom')
+    resource_class = CourseResource
 
 
 @admin.register(Course_Selection)
 class Course_SelectionAdmin(ImportExportModelAdmin):
     list_display = ('course_key', 'studentID')
+    resource_class = Course_SelectionResource
 
 @admin.register(Course_seat)
 class Course_seatAdmin(ImportExportModelAdmin):
--- loginapp/resources.py
+++ loginapp/resources.py
@@ -1,6 +1,7 @@
-from import_export import resources, fields, widgets
+from import_export import resources, fields
 from .models import Student, Classroom, Teacher, Course, Course_Selection
 from django.contrib.auth.hashers import make_password
+from import_export.widgets import ForeignKeyWidget
 class StudentResource(resources.ModelResource):
     # password = fields.Field(column_name='password', attribute='password', widget=widgets.CharWidget())
     class Meta:
@@ -18,24 +19,36 @@ class StudentResource(resources.ModelResource):
         # 調用 super 方法，執行原始的保存邏輯
         super().before_save_instance(instance, using_transactions, dry_run)
 
-
-
-class ClassroomResource(resources.ModelResource):
-    class Meta:
-        model = Classroom
-
-
 class TeacherResource(resources.ModelResource):
     class Meta:
         model = Teacher
+        import_id_fields = ['teacher_key']
 
 
 class CourseResource(resources.ModelResource):
+    teacher_key = fields.Field(
+        column_name='teacher_key',
+        attribute='teacher_key',
+        widget=ForeignKeyWidget(Teacher, 'teacher_key'))
+    
+    classroom = fields.Field(
+        column_name='classroom',
+        attribute='classroom',
+        widget=ForeignKeyWidget(Classroom, 'classroom_key'))
+
     class Meta:
         model = Course
+        fields = ('course_key', 'teacher_key', 'course_name', 'department', 'course_grade', 'course_className', 'classroom', 'seat', 'day_of_week', 'class_time')
         import_id_fields = ['course_key']
+        export_order = fields  # 保持与 fields 一致
+        export_encoding = 'utf-8-sig'  # 设置导出编码为 UTF-8
 
 class Course_SelectionResource(resources.ModelResource):
     class Meta:
         model = Course_Selection
-        
+        import_id_fields = ['ID']
+
+class ClassroomResource(resources.ModelResource):
+    class Meta:
+        model = Classroom
+        import_id_fields = ['classroom_key']
