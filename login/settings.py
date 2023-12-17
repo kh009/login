4d757ba54ed77c5f46b9d74bd37f49fef7148a20
kh009 <122242711+kh009@users.noreply.github.com>
2023-12-17 09:55:58 +0800

    12_17

--- login/settings.py
+++ login/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yos=ep8ck)$2y872cgkn&o-7mzbg=ey1ja-gl6(j7mgdebi!s6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['login-h9ln.onrender.com']
+ALLOWED_HOSTS = ['login-h9ln.onrender.com','127.0.0.1']
 
 
 # Application definition
